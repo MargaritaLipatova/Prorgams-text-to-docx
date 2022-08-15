f4295b4deef275cfabdbd9dbb39b0dd59f60664b
Margarita Lipatova <smolina.margarita2015@yandex.ru>
2022-08-15 09:24:09 +0300

    updated skript, added templape, updated readme

--- convert_to_docx.py
+++ convert_to_docx.py
@@ -1,71 +1,85 @@
 # -*- coding: utf-8 -*-
 import os
 import docx
+import glob
 from docx import Document
 from docx.shared import Pt, Cm, RGBColor 
-from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_BREAK
 from docx.oxml.ns import qn
 
+class Src2Docx():
+    
+    def __init__(self, template):
+        self.doc = Document(template)
+        section = self.doc.sections[0]
+        section.left_margin = Cm(2)
+        section.right_margin = Cm(1)
+        section.top_margin = Cm(2)
+        section.bottom_margin = Cm(2) 
+        
+    def add_heading(self, file_bn):
+        style = self.doc.styles['Heading 1']
+        style.font.name = 'Times New Roman'
+        style.font.size = Pt(14)
+        rFonts = style.element.rPr.rFonts
+        rFonts.set(qn("w:asciiTheme"), "Times New Roman")
+        rFonts.set(qn("w:hAnsiTheme"), "Times New Roman")
+        style.font.color.rgb = RGBColor(0, 0, 0)
+        head = self.doc.add_heading(file_bn, 1)
+        h_fmt = head.paragraph_format
+        h_fmt.line_spacing = 1.5
+        h_fmt.space_before = Pt(0)
+        h_fmt.space_after = Pt(0)
+        h_fmt.first_line_indent = Cm(1.5)
+        
+    def add_paragraph(self, text):
+        style = self.doc.styles['Normal']
+        style.font.size = Pt(14)
+        style.font.name = 'Times New Roman'
+        p = self.doc.add_paragraph(text_str)
+        run = p.add_run()
+        run.add_break(WD_BREAK.PAGE)
+        p_fmt = p.paragraph_format
+        p_fmt.line_spacing = 1.5
+        p_fmt.space_before = Pt(0)
+        p_fmt.space_after = Pt(0)
+        p_fmt.first_line_indent = Cm(1.5)
+        
+    def add_koll(self, texth):
+        header = self.doc.sections[0].header.paragraphs[0]
+        header.style.font.size = Pt(14)
+        header.style.font.name = 'Times New Roman'
+        header.add_run(texth)
+        header.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        
+    def save_docx(self, path):
+        self.doc.save(path)
+        
 
 cwd = os.getcwd()
 path_to_docx = os.path.join(cwd, "document.docx")
-
+koll = "ЮФКВ.00125-01 12 01"
 path_to_src = "D:\\GitHub\machine-learning-cheat-sheet\machine-learning-cheat-sheet.tex"
-name_f = os.path.basename(path_to_src)
-f = open(path_to_src,'r', encoding="utf-8")
-text_str = f.read()
-f.close()
+# name_f = os.path.basename(path_to_src)
+# f = open(path_to_src,'r', encoding="utf-8")
+# text_str = f.read()
+# f.close()
 
-# создание документа
-doc = Document()
-# задаем стиль текста по умолчанию
-style = doc.styles['Heading 1']
-# название шрифта
-style.font.name = 'Times New Roman'
-# размер шрифта
-style.font.size = Pt(14)
+path_to = "D:\\GitHub\machine-learning-cheat-sheet"
+files = glob.glob(path_to +'\*.tex')
 
-rFonts = style.element.rPr.rFonts
-rFonts.set(qn("w:asciiTheme"), "Times New Roman")
-rFonts.set(qn("w:hAnsiTheme"), "Times New Roman")
-print(style.element.xml)
-
-style.font.color.rgb = RGBColor(0, 0, 0)
-head = doc.add_heading('1.\t'+name_f, 1)
-h_fmt = head.paragraph_format
-h_fmt.line_spacing = 1.5 #Межстрочный интервал 
-h_fmt.space_before = Pt(0)
-h_fmt.space_after = Pt(0)  #интервал после абзаца.
-h_fmt.first_line_indent = Cm(1.5) #Отступ первой (красной) строки 
-
-section = doc.sections[0]
-# левое поле в сантиметрах
-section.left_margin = Cm(2)
-# правое поле в сантиметрах
-section.right_margin = Cm(1)
-# верхнее поле в сантиметрах
-section.top_margin = Cm(2)
-# нижнее поле в сантиметрах
-section.bottom_margin = Cm(2) 
-
-style = doc.styles['Normal']
-style.font.size = Pt(14)
-style.font.name = 'Times New Roman'
-
-p = doc.add_paragraph(text_str)
-# добавляем текст прогоном 
-run = p.add_run()
-# run = p.add_run('символов текста.')
-run.add_break(WD_BREAK.PAGE)
-
-p_fmt = p.paragraph_format
-p_fmt.line_spacing = 1.5 #Межстрочный интервал 
-p_fmt.space_before = Pt(0)
-p_fmt.space_after = Pt(0)  #интервал после абзаца.
-p_fmt.first_line_indent = Cm(1.5) #Отступ первой (красной) строки 
-
-
-doc.save(path_to_docx)
+docc = Src2Docx('D:\\GitHub\\Prorgams-text-to-docx\\template.docx')
+for i in files:
+    print(i)
+    name_f = os.path.basename(i)
+    f = open(i,'r', encoding="utf-8")#, errors='ignore')
+    text_str = f.read()
+    f.close()
+    
+    docc.add_heading(name_f)
+    docc.add_paragraph(text_str)
+    
+#docc.add_koll(koll)
+docc.save_docx(path_to_docx)
 
 
