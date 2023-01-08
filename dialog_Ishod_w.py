d561b782c45f9b760a471cb50d72091860bfaf3a
Alexandra_V_S <tarya.gosov@mail.ru>
2023-01-09 00:24:03 +0300

    Добавлена реакция на кнопку "Добавить файлы из папки..."

--- dialog_Ishod_w.py
+++ dialog_Ishod_w.py
@@ -5,9 +5,13 @@ Created on Wed Dec 14 23:24:09 2022
 @author: Vasilyeva
 """
 from PyQt5 import QtWidgets
-from ui_ishod_w import Ui_DialogIshodDocx  # импорт нашего сгенерированного файла
-from dialog_ChangeExtensions import dialogChangeExtensions  
+from PyQt5.QtWidgets import *
 
+#import resources
+
+from ui_files.ui_ishod_w import Ui_DialogIshodDocx  # импорт нашего сгенерированного файла
+from dialog_ChangeExtensions import dialogChangeExtensions  
+import os
 
 
 class dialogIshodDocx(QtWidgets.QDialog):
@@ -15,13 +19,19 @@ class dialogIshodDocx(QtWidgets.QDialog):
         super(dialogIshodDocx, self).__init__()
         self.ui = Ui_DialogIshodDocx()
         self.ui.setupUi(self)
-    
+        self.cwd = os.getcwd() # Получить текущее местоположение файла программы
+
+        
         # ------------------------------------------
         # кнопки
         # ------------------------------------------
+        self.ui.pBtn_UpdateTable.setToolTip("Обновить")
 
-        # подключение клик-сигнал к слоту btnClicked
+        # "Изменить расширения..."
         self.ui.pBtn_ChangeEx.clicked.connect(self.btnClicked_ChangeEx)
+        
+        # "Добавить файлы из папки..."
+        self.ui.pBtn_AddFilesInFolder.clicked.connect(self.btnClicked_AddFilesInFolder)
 
     def btnClicked_ChangeEx(self)-> None:
         """ Кнопка 'Изменить расширения...'
@@ -30,4 +40,18 @@ class dialogIshodDocx(QtWidgets.QDialog):
         _app = dialogChangeExtensions(self)
         a = _app.exec()
         print(a)
+        
+        
+    def btnClicked_AddFilesInFolder(self)-> None:
+        """ Кнопка 'Добавить файлы из папки...'
+            Returns: None
+        """
+        dir_choose = QFileDialog.getExistingDirectory(self, "Выберите папку", self.cwd) # Начальный путь
+
+        if dir_choose == "":
+            print("\ nОтменить выбор")
+            return
+
+        print("\ nВы выбрали папку:")
+        print(dir_choose)
         
