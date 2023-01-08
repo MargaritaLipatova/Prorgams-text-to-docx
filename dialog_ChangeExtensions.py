e22122ed12c914080a48225e830dc0ed7792c240
Alexandra_V_S <tarya.gosov@mail.ru>
2023-01-09 00:25:31 +0300

    Добавлена реакция на кнопки "Ок", "Отмена".
    Диалоговое окно закрывается по нажатию этих кнопок. Логика не добавлена.

--- dialog_ChangeExtensions.py
+++ dialog_ChangeExtensions.py
@@ -6,12 +6,30 @@ Created on Wed Dec 14 23:49:35 2022
 """
 
 from PyQt5 import QtWidgets
-from ui_dialogChangeExtensions import Ui_dialogChangeExtensions # импорт нашего сгенерированного файла
+from ui_files.ui_dialogChangeExtensions import Ui_dialogChangeExtensions # импорт нашего сгенерированного файла
 
 
-class dialogChangeExtensions(QtWidgets.QDialog):
+class dialogChangeExtensions(QtWidgets.QDialog, Ui_dialogChangeExtensions):
     def __init__(self, parent=None):
         super(dialogChangeExtensions, self).__init__(parent)
         self.ui = Ui_dialogChangeExtensions()
         self.ui.setupUi(self)
-         
+#        self.setupUi(self)
+        # "Изменить расширения..."
+        self.ui.pushButton_Ok.clicked.connect(self.btnClicked_Ok)
+        
+        # "Добавить файлы из папки..."
+        self.ui.pushButton_Cancel.clicked.connect(self.btnClicked_Cancel)
+        
+    def btnClicked_Ok(self)-> None:
+        """ Кнопка 'Ok'
+            Returns: None
+        """
+        self.accept()
+    
+    def btnClicked_Cancel(self)-> None:
+        """ Кнопка 'Отмена'
+            Returns: None
+        """
+        self.reject()
+    
