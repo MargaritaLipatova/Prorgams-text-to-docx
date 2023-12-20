34e0d0f83b954f1df82f3ddf02f722900cde1527
a.vasilyeva <a.vasilyeva@modulew.ru>
2023-12-20 14:54:19 +0300

    Во время предпросмотра функция create_docx перенесена в Worker.run()

--- dialog_Ishod_w.py
+++ dialog_Ishod_w.py
@@ -9,7 +9,7 @@ import subprocess
 
 from PyQt5.QtCore import QObject, Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog, QFileDialog, QFormLayout, QMessageBox
-
+from functools import partial
 import loggers
 from common import getOpenFilesAndDirs, scanDir_typeTableDir
 from convert_to_docx import Src2Docx
@@ -21,12 +21,22 @@ from ui_files.ui_ishod_w import \
 class Worker(QObject):
     finished = pyqtSignal()
     path = ".\\tmp_doc.docx"
+    pid = None # id процесса
+    __parent = None
 
     def run(self):
         """Long-running task."""
+        self.__parent.create_docx(self.path)
         p = subprocess.Popen(self.path, stdout=subprocess.PIPE, shell=True)
+        self.pid = p.pid
         p.wait()
         self.finished.emit()
+
+    def get_pid(self):
+        return self.pid
+
+    def set_parent(self, parent):
+        self.__parent = parent
 
 class dialogIshodDocx(QDialog):
     """ Главное диалоговое окно 'Исход-В'
@@ -100,71 +110,79 @@ class dialogIshodDocx(QDialog):
             где пользователь укажет место для сохранения файла.
             Returns: None
         """
-        self.loggers.info('start')
-        NameDocx = self.ui.lineEdit_NameDocx.text()
-        # NameFile = self.ui.lineEdit_NameFile.text()
-        default_filename = os.path.join(self.cwd, NameDocx)
+        try:
+            self.loggers.info('start')
+            NameDocx = self.ui.lineEdit_NameDocx.text()
+            # NameFile = self.ui.lineEdit_NameFile.text()
+            default_filename = os.path.join(self.cwd, NameDocx)
 
-        ##!!!!!!! В стадии написания и отладки
-        fileName_choose, filetype = QFileDialog.getSaveFileName(self,
-                            "Сохранение файла",
-                            default_filename, # Начальный путь
-                            "All Files (*);;Text Files (*.docx)")
+            ##!!!!!!! В стадии написания и отладки
+            fileName_choose, filetype = QFileDialog.getSaveFileName(self,
+                                            "Сохранение файла",
+                                            default_filename, # Начальный путь
+                                            "Text Files (*.docx)"
+                                            )
 
-        if fileName_choose == "":
-            self.loggers.debug("\ nОтменить выбор")
-            return
+            if fileName_choose == "":
+                self.loggers.info("\ nОтменить выбор")
+            else:
+                # Сохранить
 
-        # Сохранить
+                self.loggers.info("\ nФайл, который вы выбрали для сохранения:")
+                self.loggers.info(fileName_choose)
+                self.loggers.info(f"Тип фильтра файлов: {filetype}")
 
-        self.loggers.debug("\ nФайл, который вы выбрали для сохранения:")
-        self.loggers.debug(fileName_choose)
-        self.loggers.debug("Тип фильтра файлов:",filetype)
+                self.create_docx(fileName_choose)
+                QMessageBox.information(self, "Сохранение завершено!", "Не забудьте проверить фаил и обновить поле с количеством страниц!")
 
-        self.create_docx(fileName_choose)
-        QMessageBox.information(self, "Сохранение завершено!", "Не забудьте проверить фаил и обновить поле с количеством страниц!")
+        except Exception as err:
+            self.loggers.warning(f'Exception = {err}')
 
     def btnClicked_Preview(self)-> None:
         """ Кнопка 'Предпросмотр...'
             Открывается файл для сохранения.
         """
-        self.loggers.info('start')
-        path_to_docx = os.path.join(self.cwd, "tmp_doc.docx")
+        try:
+            self.loggers.info('start')
+            # create thread
+            self.thread = QThread()
+            # create object which will be moved to another thread
+            self.worker = Worker()
+            self.worker.set_parent(self)
+            # move object to another thread
+            self.worker.moveToThread(self.thread)
+            # after that, we can connect signals from this object to slot in GUI thread
+            # connect started signal to run method of object in another thread
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+            # start thread
+            self.thread.start()
 
-        self.create_docx(path_to_docx)
+            self.setDisabled(True)
+            self.thread.finished.connect(partial(self.setDisabled, False))
 
-       # create thread
-        self.thread = QThread()
-        # create object which will be moved to another thread
-        self.worker = Worker()
-        # move object to another thread
-        self.worker.moveToThread(self.thread)
-        # after that, we can connect signals from this object to slot in GUI thread
-        # connect started signal to run method of object in another thread
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        # start thread
-        self.thread.start()
-
-        self.setDisabled(True)
-
-        self.thread.finished.connect(lambda: self.setDisabled(False))
+        except Exception as err:
+            self.loggers.warning(f'Exception = {err}')
 
     def create_docx(self, path_to_docx)-> None:
         """ Создание/сохранение файла
         Args:
             path_to_docx (_type_): путь к файлу
         """
-        self.loggers.info('start')
-        name_doc = self.ui.lineEdit_NameFile.text().upper() #должен быть заглавными буквами, когда окажется в документе
-        name_num_dec = self.ui.lineEdit_NameDocx.text().upper()
-        files = self.tvSourceCodeFiles.getDocxListTable()
+        try:
+            self.loggers.info('start')
+            name_doc = self.ui.lineEdit_NameFile.text().upper() #должен быть заглавными буквами, когда окажется в документе
+            name_num_dec = self.ui.lineEdit_NameDocx.text().upper()
+            files = self.tvSourceCodeFiles.getDocxListTable()
 
-        docc = Src2Docx('.\\auxiliary\\template.docx', name_doc, name_num_dec)
+            docc = Src2Docx('.\\auxiliary\\template.docx', name_doc, name_num_dec)
 
-        docc.add_files(files)
-        docc.add_koll(name_num_dec)
-        docc.add_table_lri()
-        docc.save_docx(path_to_docx)
+            docc.add_files(files)
+            docc.add_koll(name_num_dec)
+            docc.add_table_lri()
+            docc.save_docx(path_to_docx)
+            self.loggers.info('end')
+        except Exception as err:
+            self.loggers.warning(f'Exception = {err}')
